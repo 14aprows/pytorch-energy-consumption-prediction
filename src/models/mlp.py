fdba16a8a49e876999735e207ee84fa4354c5b7b
Veerman14 <hasibuanfirman1404@gmail.com>
2026-02-04 21:06:28 +0700

    add baseline MLPRegressor

---
+++ src/models/mlp.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+
+class MLPRegressor(nn.Module):
+    def __init__(self, input_dim):
+        super().__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(input_dim, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+
+            nn.Linear(128, 64),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+
+            nn.Linear(64, 32),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
+
+            nn.Linear(32, 1)
+        )
+
+    def forward(self, x):
+        return self.model(x)
